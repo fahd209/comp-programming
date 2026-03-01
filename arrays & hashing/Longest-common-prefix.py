c7e4c0fbb27b6c31a3502f965490b0b0c9a0eef8
Fahd <fahds2005@icloud.com>
2026-03-01 11:27:27 -0800

    arrays & hashing: longest common prefix (review)

--- arrays & hashing/Longest-common-prefix.py
+++ arrays & hashing/Longest-common-prefix.py
@@ -18,6 +18,76 @@ class Solution(object):
                 j -= 1
 
         return prefix[:j + 1]
+    
+
+# review
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        """
+        understanding:
+            input:
+                arrays: strs
+
+            output:
+                a string that has the longest common prefix of all strings
+
+        approach:
+            if the array is empty return an empty string
+
+            first example
+            prefix = strs[0]
+            ["bat","bag","bank","band", "sand"]
+        """
+
+        if strs == []:
+            return ""
+
+        prefix = strs[0]
+
+        for s in strs:
+            while not s.startswith(prefix):
+                if prefix != "":
+                    prefix = prefix[:len(prefix) - 1]
+                else:  
+                    return ""
+
+        return prefix
+    
+# review: optimized
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        """
+        understanding:
+            input:
+                arrays: strs
+
+            output:
+                a string that has the longest common prefix of all strings
+
+        approach:
+            if the array is empty return an empty string
+
+            first example
+            prefix = strs[0]
+            ["bat","bag","bank","band", "sand"]
+                                        ""
+        
+        time: O(1)
+        space: O(n)
+        """
+
+        if strs == []:
+            return ""
+
+        prefix = strs[0]
+        j = len(prefix) - 1
+        for s in strs:
+            while not s.startswith(prefix[:j+1]):
+                if j == 0:
+                    return ""
+                j -= 1
+
+        return prefix[:j+1]
                 
 
         
